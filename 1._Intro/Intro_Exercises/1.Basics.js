ddd89f34b67a68f82823a6ead07978b74b38a376
gustavvingtoftandersen <gust2968@stud.kea.dk>
2020-02-06 09:51:32 +0100

    Added the first excersises and second file

---
+++ 1._Intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,9 @@
+var firstName = "Gustav"
+var lastName = "Vingtoft"
+
+console.log('My first name is', firstName, 'and my last name is', lastName);
+
+var year = "2017";
+var number = 1;
+
+console.log(Number(year) + number)
